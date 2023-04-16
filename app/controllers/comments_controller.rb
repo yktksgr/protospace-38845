231297519ced661f4c8fafd4f14df7364309acba
yktksgr <ymmt.yshr1006@gmail.com>
2023-04-16 11:59:31 +0900

    修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create
    @comment = Comment.new(comment_params)
    if @comment.save
-    redirect_to root_path
+    redirect_to prototype_path(@comment.prototype)
    else
     @prototype = @comment.prototype
     @comments = @prototype.comments
